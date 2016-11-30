e61aaf1593a2a9a86deb484665df4134260becb1
Kennytian <kenny_tian@outlook.com>
2016-11-30 23:20:52 +0800

    Add image scale sample

--- pages/message/index.js
+++ pages/message/index.js
@@ -9,7 +9,11 @@ Page({
       {url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg'},
       {url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'}
     ],
-    names: []
+    names: [],
+
+    firstSrc: 'http://bpic.588ku.com/back_pic/00/03/85/1656205138bbe2d.png',//图片链接
+    secondSrc: 'http://bpic.588ku.com/back_pic/04/07/63/28581203949ca9d.jpg!/fw/400/quality/90/unsharp/true/compress/true',//图片链接
+    thirdSrc: 'http://img1.gtimg.com/ent/pics/hv1/13/71/2061/134034643.jpg',
   },
 
   onLoad(options) {
@@ -27,8 +31,8 @@ Page({
     wx.request({
       url: 'http://news-at.zhihu.com/api/4/news/latest',
       success(resp){
-        let result  = Immutable.fromJS(resp);
-        let stories =  result.get('data').get('stories').filter(item=>{
+        let result = Immutable.fromJS(resp);
+        let stories = result.get('data').get('stories').filter(item => {
           return item.get('title')
           // return item.toJS().title
         });
@@ -38,6 +42,27 @@ Page({
         // })
       }
     });
+  },
+  imageLoad1(e){
+    console.debug('print-image1',e);
+    let imageSize = util.imageScale(e);
+    this.setData({
+      firstWH: `width:${imageSize.imageWidth}px;height:${imageSize.imageHeight}px;`
+    })
+  },
+  imageLoad2(e){
+    console.debug('print-image2',e);
+    let imageSize = util.imageScale(e);
+    this.setData({
+      secondWH: `width:${imageSize.imageWidth}px;height:${imageSize.imageHeight}px;`
+    })
+  },
+  imageLoad3(e){
+    console.debug('print-image3',e);
+    let imageSize = util.imageScale(e);
+    this.setData({
+      thirdWH: `width:${imageSize.imageWidth}px;height:${imageSize.imageHeight}px;`
+    })
   },
   onHide(){
     console.debug('4.onHide', util.formatTime(new Date()));
